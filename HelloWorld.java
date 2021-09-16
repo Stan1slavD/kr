e843ebb0cf3bd4a11fa95fe93ee67c32cd07ac71
StanislavD <stasdamaratskiy@gmail.com>
2021-09-16 11:30:10 +0300

    add \n

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class Hello
 {
   public static void main(String[] args)
   {
-    System.out.print("Hello, world!");
+    System.out.print("Hello, world!\n");
     System.out.print("New Feature!");
   }
 }
