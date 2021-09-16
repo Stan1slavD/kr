59db88b6b558b56933878ae267d69d0820782f7c
StanislavD <stasdamaratskiy@gmail.com>
2021-09-16 11:30:10 +0300

    4.4

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class Hello
 {
   public static void main(String[] args)
   {
-    System.out.print("Hello, world!1");
+    System.out.print("Hello, world!1\n");
     System.out.print("New Feature!");
   }
 }
